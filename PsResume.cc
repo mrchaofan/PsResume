8afbdf37008eb45254cb65b7ea5c00775aef093f
supericeliu <24412720+mrchaofan@users.noreply.github.com>
2022-05-07 00:05:24 +0800

    Initial commit

---
+++ PsResume.cc
@@ -0,0 +1,22 @@
+﻿#include <windows.h>
+
+typedef NTSTATUS(NTAPI* NtResumeProcess)(IN HANDLE ProcessHandle);
+
+static NtResumeProcess pfnNtResumeProcess = (NtResumeProcess)GetProcAddress(
+	GetModuleHandle(L"ntdll"), "NtResumeProcess");
+
+static inline NTSTATUS ResumeProcess(DWORD pid)
+{
+	HANDLE processHandle = OpenProcess(PROCESS_SUSPEND_RESUME, FALSE, pid);
+	NTSTATUS status = pfnNtResumeProcess(processHandle);
+	CloseHandle(processHandle);
+	return status;
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc == 2) {
+		DWORD pid = atoi(argv[1]);
+		return ResumeProcess(pid);
+	}
+}
